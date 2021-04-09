7345b376cf38e81464d8d990bb085f2dd151d912
marcinch2004 <marcinch2004@yahoo.ie>
2021-04-09 14:41:53 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-100px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.top = "-100px";
+  } else {
+    document.getElementById("navbar").style.top = "0";
+  }
+}
